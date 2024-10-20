e437b33ea01a663b6117c5012d254c57e171d4db
Tomasz Jurek <jur.tomasz13@gmail.com>
2024-10-20 11:27:16 +0200

    update(structure): rename react->frontend, nest->backend

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  '{apps,libs,tools}/**/*.{ts,tsx}': files => {
+  '{apps,libs}/**/*.{ts,tsx}': files => {
     return `nx affected --target=typecheck --files=${files.join(',')}`;
   },
-  '{apps,libs,tools}/**/*.{js,ts,jsx,tsx,json}': [
+  '{apps,libs}/**/*.{js,ts,jsx,tsx,json}': [
     files => `nx affected:lint --files=${files.join(',')}`,
   ],
 };
